9058e92ed9f8da8e8b4be61a7c98ced8e268f7ea
deEliaJuanManuel <juanma21042104@gmail.com>
2025-05-12 17:30:33 -0300

    Unidad2-Cap1-Lab1-terminado

---
+++ Ejercicio 8/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("La contraseña a ingresar es: Clave123");
+
+for (int intento = 0; intento < 4; intento++)
+{
+    Console.Write("Ingrese la contraseña: ");
+    string clave = Console.ReadLine();
+    if (clave == "Clave123")
+    {
+        Console.WriteLine("Contraseña correcta");
+        break;
+    }
+    else
+    {
+        if (intento == 3)
+        {
+            Console.WriteLine("Contraseña incorrecta. Ha agotado los intentos.");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Contraseña incorrecta. Intente nuevamente.");
+        }
+        
+       
+    }
+}
